581a09c8eda806dc111c01d539d2a112c9630e19
github username <github email address>
2017-06-15 17:34:56 -0400

    started bootstrap styling

--- client/gregslist-client/src/components/PostsList.js
+++ client/gregslist-client/src/components/PostsList.js
@@ -4,8 +4,24 @@ import {Route, Switch, Link} from 'react-router-dom'
 export default function PostsList(props){
   return(
     <div>
-      {props.posts.map(post => <Link to={`/posts/${post.id}`}><img src={post.img_url}/></Link>)}
-      <Link to='/posts/new'>New Post</Link>
+      <div className='container'>
+        <div className='flex-row row'>
+          <div className='col-sm-12'>
+            <div className='row'>
+              {props.posts.map(post => <div className='col-sm-3'><Link to={`/posts/${post.id}`}><img className='img-thumbnail' src={post.img_url}/></Link></div>)}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-md-5'>
+        </div>
+          <div className='col-md-2'>
+            <Link className="btn btn-primary" to='/posts/new'>New Post</Link>
+          </div>
+          <div className='col-md-5'>
+        </div>
+      </div>
     </div>
   )
 }
