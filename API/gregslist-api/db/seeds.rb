97804068a86a1c30998a8468585d776e379fc944
github username <github email address>
2017-06-15 22:57:39 -0400

    cardFlippingTop

--- API/gregslist-api/db/seeds.rb
+++ API/gregslist-api/db/seeds.rb
@@ -19,17 +19,6 @@ Post.create([
       email: "minionlover69@hotmail.com",
       value: 60.70
 },
-    {title:"Pre 1800 antiques",
-      description: "I am looking for old Mormon artifacts from the book of Mormon. I would like to purchase items from the days of the Nephites and Lamanites such as shields, arrows, bows, knives, or any of the other items that they might have used in there battles, anything would be great. I would also love to have maps, pottery, coins, statues, vases, or any artifacts from the large city of Zarahemla or Mulek. If some one had any writing in reformed Egyptian that was apparently used I would like to see it.
-It should be easy to find at least one item from a civilization that had millions of people with cities and large army’s. The picture I included was directly off of the LDS website and it clearly shows swords, spears, breastplates, and shields. All that takes a developed people to make.
-
-Also if any LDS people are out there that were dark skinned and are now changed to “white and delightsome” it would be neat to here your story. I would pay top dollar to any native American that could prove his DNA was from a Jewish background. In 1857, Brigham Young declared that apostates would “become gray-haired, wrinkled, and black, just like the Devil” (Journal of Discourse 5:332). Any apostates out there that this has happened to!! I would give $10,000 for an interview.
-
-I am not interested in any biblical historical items, as they are not that hard to find, I am only interested in Mormon (pre 1800's) items.",
-      img_url: "https://www.craigslist.org/about/best/eid/5594898310-1.jpg",
-      email: "oldfurnitureperson@gmail.com",
-      value: 99.99
-},
     {title: "FREE Baby Zombie Hand FREE",
       description: "Name: Melvin the baby zombie hand
 Likes: women’s panties (preferably with words), holding jewelry and driving Ford Mustangs
@@ -53,5 +42,87 @@ I've included photos, to show his gnarly awesomeness, and I hope you can help pr
       img_url: "https://www.craigslist.org/about/best/sea/6089109342-1.jpg",
       email: "p07a70@yahoo.com",
       value: 10.30
-    }
+    },
+    {title: "Used Pirate ship",
+      description: "You arr lookin at a used Pirate ship for sale by the ownarr she is equipped with 3 cannons and a skeleton crew! Please, serious and qualified Pirates can parlay with Captain Dan",
+      img_url: "https://www.craigslist.org/about/best/ric/5547543805-1.jpg",
+      email: "captaindan@yahoo.com",
+      value: 45.60
+},
+    {title: "Minion!",
+      description: "Handmade Minion fire pit. 30 tall, with poker and pick. $225 or interesting trades. Mostly interested in tools, hand held man toys and automotive/motorcycle related things, but willing to look at anything.",
+      img_url: "https://www.craigslist.org/about/best/ric/5862430234-1.jpg",
+      email: "minionlover69@hotmail.com",
+      value: 60.70
+},
+    {title: "FREE Baby Zombie Hand FREE",
+      description: "Name: Melvin the baby zombie hand
+Likes: women’s panties (preferably with words), holding jewelry and driving Ford Mustangs
+Eats: peanuts and human flesh
+FREE family friendly fun!!!",
+      img_url: "https://www.craigslist.org/about/best/sdo/5556783245-5.jpg",
+      email: "zombiedude@gmail.com",
+      value: 30.20
+},
+    {title: "Glorious Starter Potato",
+      description: "In spring cleaning, I was cleaning out a back cabinet and found a glorious starter potato. I feel sad just chucking him since he has made such an impression in attempt at life and propagation. If you have a yard, or garden, and would be willing to take this glorious plant from my hands, I'd be happy, knowing he is able to live his dreams.
+
+I've included photos, to show his gnarly awesomeness, and I hope you can help provide him a great loving home aka fertile soil to grow.",
+      img_url: "https://www.craigslist.org/about/best/sea/6089109342-1.jpg",
+      email: "p07a70@yahoo.com",
+      value: 10.30
+    },
+    {title: "Mobile brothel",
+      description: "197? Chrysler motor home. I bought this former mobile brothel from one of the oldest pimps in Nevada. I believe it’s the one I was conceived and born in. My mother found it for me a few years ago while volunteering for the humane society in Vegas. I bought it right away for $10,000 because it’s kind of sentimental to us since my father was an aeronautical engineer turned Navy Seal firefighter and a CIA spy who worked for Richard Nixon himself. Needless to say didn’t have much time for dating so he would visit my mom on his rare time off from work in this mobile brothel that she worked in while she saved up money for college. Anyways my mom and I hoped to restore it to it’s former glory days and get her and it back on the road. But after applying for permits, we were denied for a massive crab infestation and mold issues. It starts and runs well. Steers straight and is good in gas. I’d like to get around $10k for it as is. Clean title in hand. Thanks.
+P.S. Dad if you are reading this please email me as i would love to meet you someday.",
+      img_url: "https://www.craigslist.org/about/best/den/5458791945-1.jpg",
+      email: "missmona@yahoo.com",
+      value: 40.00
+    },
+    {title: "Motorized Bicycle",
+      description: "I regret it but selling my custom built Motorized Bicycle, please look at the photos for more details.
+
+It has a Honda 4 stroke GXH50 49.4cc 2.5hp engine on a SPECIALIZED HARD ROCK 21spd
+High end mountain bike with Weinman Rims.
+
+Note the 3 to 1 ratio Torq-A-Verter TAV2 model which allows for good climbing power and idle at stops.
+
+I built this unit for metal detecting days and traveling down fire lane roads and going across corn fields so it has a lot of low in power plus a good top end speed for down the road cursing with the Torq-A-Verter.
+
+Best of all it is a 4 stroke engine so no mixing oil in the gas.
+
+Note the bike frame has not been altered so take it apart and its back to a stock bike or pop the drive chain and it's a pedal bike.
+
+I will respond to all messages sent to me from Craigs List and who give me your contact phone number.
+
+This is also listed on Ebay [...] just in case you trust buying there more than here.
+
+Pick up in Kenosha Wisconsin as I will not ship. I do take verified PayPal and will help with trucking pick up but will not crate in case you want to arrange shipping pick up.",
+      img_url: "https://www.craigslist.org/about/best/rac/5579956763-3.jpg",
+      email: "missmona@yahoo.com",
+      value: 40.00
+    },
+    {title: "Motorized Bicycle",
+      description: "I regret it but selling my custom built Motorized Bicycle, please look at the photos for more details.
+
+It has a Honda 4 stroke GXH50 49.4cc 2.5hp engine on a SPECIALIZED HARD ROCK 21spd
+High end mountain bike with Weinman Rims.
+
+Note the 3 to 1 ratio Torq-A-Verter TAV2 model which allows for good climbing power and idle at stops.
+
+I built this unit for metal detecting days and traveling down fire lane roads and going across corn fields so it has a lot of low in power plus a good top end speed for down the road cursing with the Torq-A-Verter.
+
+Best of all it is a 4 stroke engine so no mixing oil in the gas.
+
+Note the bike frame has not been altered so take it apart and its back to a stock bike or pop the drive chain and it's a pedal bike.
+
+I will respond to all messages sent to me from Craigs List and who give me your contact phone number.
+
+This is also listed on Ebay [...] just in case you trust buying there more than here.
+
+Pick up in Kenosha Wisconsin as I will not ship. I do take verified PayPal and will help with trucking pick up but will not crate in case you want to arrange shipping pick up.",
+      img_url: "http://smileyfaceplace.weebly.com/uploads/6/9/7/1/6971266/4692542_orig.jpg",
+      email: "missmona@yahoo.com",
+      value: 40.00
+}
 ])
