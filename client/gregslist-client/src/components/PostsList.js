f05f9d8f9c2b24ed5226a414ffd1e60c378d72ab
github username <github email address>
2017-06-15 22:31:32 -0400

    almostThere

--- client/gregslist-client/src/components/PostsList.js
+++ client/gregslist-client/src/components/PostsList.js
@@ -5,11 +5,25 @@ export default function PostsList(props){
   return(
     <div>
 
-        <div className='flex-row row'>
-            <div className='row'>
-              {props.posts.map(post => <div className='col-sm-2'><Link to={`/posts/${post.id}`}><img className='img' src={post.img_url}/></Link></div>)}
-            </div>
-        </div>
+          <div className='flex-row row'>
+              <div className='row'>
+                    {props.posts.map(post =>
+                      <div id="f1_container">
+                        <div id="f1_card" class="shadow">
+                            <div className='col-sm-2 front face'><Link to={`/posts/${post.id}`}>
+                              <img className='img' src={post.img_url}/></Link>
+                            </div>
+                            <div class="back face center">
+                              <p>{post.title}</p>
+                            </div>
+                          </div>
+                    </div>
+                          )
+
+                    }
+                </div>
+              </div>
+
 
       <div className='row'>
         <div className='col-md-5'>
