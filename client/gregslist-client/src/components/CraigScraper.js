d5b1d30e23530c60e30ad01d18477a1cee61f529
github username <github email address>
2017-06-18 14:05:19 -0400

    CL_data_in_container

--- client/gregslist-client/src/components/CraigScraper.js
+++ client/gregslist-client/src/components/CraigScraper.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 const craigslist = require('node-craigslist');
-const responses = []
-
 
 
 const client = new craigslist.Client({
@@ -12,18 +10,19 @@ export default class CraigScraper extends Component {
   constructor(){
     super()
     this.state = {
-      lists: []
+      lists: ''
     }
-    this.getLists = this.getLists.bind(this)
+    this.searchCraigsList = this.searchCraigsList.bind(this)
   }
 
 
-  getLists() {
+  searchCraigsList() {
     client
       .search("drums")
       .then( (listings) => {
         listings.forEach( (listing) => {
           client.details(listing)
+          .then( res => JSON.stringify(res))
           .then( (response) => this.setState( (prevState) => {
               return {
                   lists: [...prevState.lists, response]
@@ -38,25 +37,13 @@ export default class CraigScraper extends Component {
       )
     }
 
-        // client
-  //   .search("drums")
-  //   .then((listings) => {
-  //     listings.forEach( (listing) => (client.details(listing))
-  //     .then((response) => this.setState((prevState) => {
-  //       return
-  //         lists: [...prevState, response]
-  //     })
-  //   )
-  //   )}
-  //   .catch((err) => {
-  //     console.error(err);
-  //   });
+
 
 
 
 
   componentDidMount(){
-    this.getLists()
+    // this.searchCraigsList()
   }
 
   render(){
